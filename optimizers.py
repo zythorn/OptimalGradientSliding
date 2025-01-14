f4030cbaa1850fe37242a224de2594956c536f33
Aleksei Khalin <alexeyx0418@gmail.com>
2025-01-14 19:05:20 +0300

    Better file structure, cleaned up

--- optimizers.py
+++ optimizers.py
@@ -22,8 +22,7 @@ class GradientDescent(BaseFirstOrderOptimizer):
         for i in range(n_iters):
             self.x = self.x - 1. / self.L * self.grad_f(self.x)
             if self.log and (i + 1) % 50 == 0:
-                print(f"Error after {i + 1} steps: {self.f(self.x)}")
-                print(f"Gradient: {self.grad_f(self.x)[:5]}")
+                print(f"Function value after {i + 1} steps: {self.f(self.x)}")
         return self.x
 
 class AcceleratedGradientDescent(BaseFirstOrderOptimizer):
@@ -46,7 +45,7 @@ class AcceleratedGradientDescent(BaseFirstOrderOptimizer):
             if self.log:
                 self.x_history.append(self.x)
                 if (i + 1) % 50 == 0:
-                    print(f"Error after {i + 1} steps: {self.f(self.x)}")
+                    print(f"Function value after {i + 1} steps: {self.f(self.x)}")
 
         if self.log:
             return self.x_history
@@ -79,34 +78,12 @@ class OGMG(BaseFirstOrderOptimizer):
 
             self.x = y_new + y_coef * (y_new - self.y) + x_coef * (y_new - self.x)
             self.y = y_new
-            if self.log and i % 10 == 0:
-                print(f"Error after {i} steps: {self.f(self.x)}")
+            if self.log and i % 50 == 0:
+                print(f"Function value after {i} steps: {self.f(self.x)}")
 
         return self.x
 
-class BaseAdditiveOptimizer(ABC):
-    def __init__(self, q: Callable[[np.ndarray], np.ndarray],
-                 grad_q: Callable[[np.ndarray], np.ndarray],
-                 p: Callable[[np.ndarray], np.ndarray],
-                 grad_p: Callable[[np.ndarray], np.ndarray],
-                 x_init: np.ndarray, mu: float,
-                 liepshitz_q: float, liepshitz_p: float,
-                 log: bool=False):
-        self.q = q
-        self.grad_q = grad_q
-        self.p = p
-        self.grad_p = grad_p
-        self.liepshitz_q = liepshitz_q
-        self.liepshitz_p = liepshitz_p
-        self.mu = mu
-        self.x = x_init
-        self.log = log
-
-    @abstractmethod
-    def optimize(self, n_iters: int=10**3) -> np.ndarray:
-        raise NotImplementedError()
-
-class AcceleratedExtraGradient(BaseAdditiveOptimizer):
+class AcceleratedExtraGradient():
     def __init__(self, q: Callable[[np.ndarray], np.ndarray],
                  grad_q: Callable[[np.ndarray], np.ndarray],
                  p: Callable[[np.ndarray], np.ndarray],
@@ -115,13 +92,25 @@ class AcceleratedExtraGradient(BaseAdditiveOptimizer):
                  x_init: np.ndarray, mu: float,
                  liepshitz_q: float, liepshitz_p: float,
                  log: bool=False):
-        super().__init__(q, grad_q, p, grad_p, x_init, mu, liepshitz_q, liepshitz_p, log)
+        self.q = q
+        self.grad_q = grad_q
+        self.p = p
+        self.grad_p = grad_p
+
+        self.liepshitz_q = liepshitz_q
+        self.liepshitz_p = liepshitz_p
+        self.mu = mu
+
+        self.x = x_init
         self.x_g = self.x_f = x_init
         self.auxiliary_opt = auxiliary_opt
+
         self.tau = np.min((1., np.sqrt(mu) / (2 * np.sqrt(liepshitz_p))))
         self.theta = 1. / (2 * liepshitz_p)
         self.eta = np.min((1. / (2 * mu), (1. / (2 * np.sqrt(mu * liepshitz_p)))))
         self.alpha = mu
+
+        self.log = log
 
         if self.log:
             self.x_history = [x_init]
@@ -167,62 +156,8 @@ class AcceleratedExtraGradient(BaseAdditiveOptimizer):
             if self.log:
                 self.x_history.append(self.x)
                 if (i + 1) % 50 == 0:
-                    print(f"Error after {i + 1} steps: {self.p(self.x) + self.q(self.x)}")
+                    print(f"Function value after {i + 1} steps: {self.p(self.x) + self.q(self.x)}")
 
         if self.log:
             return self.x_history
-        return self.x
-
-class DANE():
-    def __init__(self, f: Callable[[np.ndarray, int], np.ndarray],
-                 grad_f: Callable[[np.ndarray, int], np.ndarray],
-                 auxiliary_opt: BaseFirstOrderOptimizer,
-                 x_init: np.ndarray, mu: float, liepshitz_const: float,
-                 lr: float, regularizer: float, num_workers: int,
-                 log: bool=False):
-        self.f = f
-        self.grad_f = grad_f
-        self.aux_opt = auxiliary_opt
-        self.x = x_init
-        self.local_x = []
-        self.local_grads = []
-
-        self.mu = mu
-        self.L = liepshitz_const
-        self.lr = lr
-        self.regularizer = regularizer
-        self.num_workers = num_workers
-
-        self.log = log
-
-    def _auxiliary_problem(self, worker: int) -> tuple[Callable[[np.ndarray], np.ndarray],
-                                                       Callable[[np.ndarray], np.ndarray],
-                                                       np.ndarray]:
-        mean_grad = sum(self.local_grads) / self.num_workers
-        def f(x: np.ndarray) -> np.ndarray:
-            return (self.f(x, worker) - np.dot(self.local_grads[worker] - self.lr * mean_grad, x) + 
-                    self.regularizer / 2.  * np.dot(x - self.x, x - self.x))
-
-        def grad_f(x: np.ndarray) -> np.ndarray:
-            return (self.grad_f(x, worker) - (self.local_grads[worker] - self.lr * mean_grad) + 
-                    self.regularizer * (x - self.x))
-
-        return f, grad_f
-
-    def optimize(self, n_iters: int=10**3) -> np.ndarray:
-        for i in range(n_iters):
-            self.local_grads = []
-            for worker in range(self.num_workers):
-                self.local_grads.append(self.grad_f(self.x, worker))
-            for worker in range(self.num_workers):
-                f, grad_f = self._auxiliary_problem(worker)
-                aux_opt = self.aux_opt(f, grad_f, self.x, self.L + self.mu, self.mu, True)
-                self.local_x.append(aux_opt.optimize(10))
-            self.x = sum(self.local_x) / self.num_workers
-            if self.log and (i + 1) % 5 == 0:
-                sum_f: float = 0
-                for worker in range(self.num_workers):
-                    sum_f = sum_f + self.f(self.x, worker)
-                print(f"Error after {i + 1} steps: {sum_f}")
-        
         return self.x
---
+++ problems.py
@@ -0,0 +1,136 @@
+import numpy as np
+
+class RidgeRegression():
+    def __init__(self, X: np.ndarray, y: np.ndarray, lmbd: float):
+        self.X = X
+        self.y = y
+        self.lmbd = lmbd
+
+    def f(self, x: np.ndarray) -> np.ndarray:
+        diff = np.dot(self.X, x) - self.y
+        return np.dot(diff, diff)
+
+    def g(self, x: np.ndarray) -> np.ndarray:
+        return self.lmbd / 2. * np.dot(x, x)
+
+    def grad_f(self, x: np.ndarray) -> np.ndarray:
+        diff = self.X @ x - self.y
+        return 2 * (self.X.T @ diff)
+
+    def grad_g(self, x: np.ndarray) -> np.ndarray:
+        return self.lmbd * x
+
+class RandomDistributedRidgeRegression():
+    def __init__(self, dim: int, data_size: int, lmbd: float, 
+                 gaussian_sigma: float=1., num_workers: int=1, seed: int=42):
+        np.random.seed(seed)
+        self.x_clean = np.random.rand(data_size, dim) * 2. - 1.
+        self.y_clean = np.random.randn(data_size) * 2. - 1.
+
+        self.x_noise = np.random.randn(num_workers, data_size, dim) * gaussian_sigma
+        self.y_noise = np.random.randn(num_workers, data_size) * gaussian_sigma
+
+        self.x = self.x_noise + self.x_clean
+        self.y = self.y_noise + self.y_clean
+
+        self.data_size = data_size
+        self.num_workers = num_workers
+        self.lmbd = lmbd
+
+    def f_at_node(self, x: np.ndarray, node: int) -> np.ndarray:
+        diff = np.dot(self.x[node], x) - self.y[node]
+        return np.mean(diff ** 2) / 2. + self.lmbd / 2. * np.dot(x, x)
+
+    def grad_f_at_node(self, x: np.ndarray, node: int) -> np.ndarray:
+        diff = np.dot(self.x[node], x) - self.y[node]
+        return np.dot(diff, self.x[node]) / self.data_size + self.lmbd * x
+
+    def q(self, x: np.ndarray) -> np.ndarray:
+        return self.f_at_node(x, 0)
+
+    def p(self, x: np.ndarray) -> np.ndarray:
+        value: float = 0.
+        f_master = self.f_at_node(x, 0)
+        for node in range(1, self.num_workers):
+            value += self.f_at_node(x, node) - f_master
+        return value / self.num_workers
+
+    def r(self, x: np.ndarray) -> np.ndarray:
+        value: float = 0.
+        for node in range(self.num_workers):
+            value += self.f_at_node(x, node)
+        return value / self.num_workers
+
+    def grad_q(self, x: np.ndarray) -> np.ndarray:
+        return self.grad_f_at_node(x, 0)
+
+    def grad_p(self, x: np.ndarray) -> np.ndarray:
+        grad = np.zeros_like(x, dtype=float)
+        grad_master = self.grad_f_at_node(x, 0)
+        for node in range(1, self.num_workers):
+            grad += self.grad_f_at_node(x, node) - grad_master
+        return grad / self.num_workers
+
+    def grad_r(self, x: np.ndarray) -> np.ndarray:
+        grad = np.zeros_like(x, dtype=float)
+        for node in range(self.num_workers):
+            grad += self.grad_f_at_node(x, node)
+        return grad / self.num_workers
+
+class DistributedRidgeRegression():
+    def __init__(self, x_clean: np.ndarray, y_clean: np.ndarray, lmbd: float,
+                 gaussian_sigma: float=0.01, num_workers: int=1, seed: int=42):
+        np.random.seed(seed)
+        # x_clean = x_clean[:300, :300]
+        # y_clean = y_clean[:300]
+        self.data_size, self.dim = x_clean.shape
+
+        self.x_noise = np.random.randn(num_workers, self.data_size, self.dim) * gaussian_sigma
+        self.y_noise = np.random.randn(num_workers, self.data_size) * gaussian_sigma
+
+        self.x = self.x_noise + x_clean
+        self.y = self.y_noise + y_clean
+
+        self.data_size = self.data_size
+        self.num_workers = num_workers
+        self.lmbd = lmbd
+
+    def f_at_node(self, x: np.ndarray, node: int) -> np.ndarray:
+        diff = np.dot(self.x[node], x) - self.y[node]
+        return np.mean(diff ** 2) / 2. + self.lmbd / 2. * np.dot(x, x)
+
+    def grad_f_at_node(self, x: np.ndarray, node: int) -> np.ndarray:
+        diff = np.dot(self.x[node], x) - self.y[node]
+        return np.dot(diff, self.x[node]) / self.data_size + self.lmbd * x
+
+    def q(self, x: np.ndarray) -> np.ndarray:
+        return self.f_at_node(x, 0)
+
+    def p(self, x: np.ndarray) -> np.ndarray:
+        value: float = 0.
+        f_master = self.f_at_node(x, 0)
+        for node in range(1, self.num_workers):
+            value += self.f_at_node(x, node) - f_master
+        return value / self.num_workers
+
+    def r(self, x: np.ndarray) -> np.ndarray:
+        value: float = 0.
+        for node in range(self.num_workers):
+            value += self.f_at_node(x, node)
+        return value / self.num_workers
+
+    def grad_q(self, x: np.ndarray) -> np.ndarray:
+        return self.grad_f_at_node(x, 0)
+
+    def grad_p(self, x: np.ndarray) -> np.ndarray:
+        grad = np.zeros_like(x, dtype=float)
+        grad_master = self.grad_f_at_node(x, 0)
+        for node in range(1, self.num_workers):
+            grad += self.grad_f_at_node(x, node) - grad_master
+        return grad / self.num_workers
+
+    def grad_r(self, x: np.ndarray) -> np.ndarray:
+        grad = np.zeros_like(x, dtype=float)
+        for node in range(self.num_workers):
+            grad += self.grad_f_at_node(x, node)
+        return grad / self.num_workers
